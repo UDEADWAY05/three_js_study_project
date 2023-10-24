90c0b8565a4637cbca91fb9f86317381712bb9af
Dmitry Gerasimov <udeadway523@gmail.com>
2023-10-24 20:29:24 +0300

    add 'Earth' and 'Start' the 24th of October - this is Finaly!

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -20,11 +20,13 @@ const Contact = () => {
     const [loading, setLoading] = useState(false)
     
     const handleChange = (e) => {
-
+        const { name, value } = e.target
+        setForm({ ...form, [name]: value })
     }
 
     const handleSubmit = (e) => {
-        
+        e.preventDefault();
+        console.log(form)
     }
 
     return (
@@ -37,8 +39,67 @@ const Contact = () => {
                     onSubmit={handleSubmit}
                     className='mt-12 flex flex-col gap-8'
                 >
-
+                    <label className='flex flex-col'>
+                        <span className='text-white font-medium mb-4'>
+                            Your Name
+                        </span>
+                        <input
+                            type="text"
+                            name="name"
+                            value={form.name}
+                            onChange={handleChange}
+                            placeholder="What's your name ?"
+                            className='bg-tertiary py-4 px-6 
+                            placeholder:text-secondary rounded-lg 
+                            outlined-none border-none font-medium'
+                        />
+                    </label>
+                    <label className='flex flex-col'>
+                        <span className='text-white font-medium mb-4'>
+                            Your Email
+                        </span>
+                        <input
+                            type="text"
+                            name="email"
+                            value={form.email}
+                            onChange={handleChange}
+                            placeholder="What's your Email ?"
+                            className='bg-tertiary py-4 px-6 
+                            placeholder:text-secondary rounded-lg 
+                            outlined-none border-none font-medium'
+                        />
+                    </label>
+                    <label className='flex flex-col'>
+                        <span className='text-white font-medium mb-4'>
+                            Your Message
+                        </span>
+                        <textarea
+                            rows="7"
+                            name="message"
+                            value={form.message}
+                            onChange={handleChange}
+                            placeholder="What do you want to say ?"
+                            className='bg-tertiary py-4 px-6 
+                            placeholder:text-secondary rounded-lg 
+                            outlined-none border-none font-medium'
+                        />
+                    </label>
+                    <button
+                        type='submit'
+                        className='bg-tertiary py-3 px-8 
+                        outline-none w-fit text-white font-bold 
+                        shadow-md shadow-primary rounded-xl'
+                    >
+                        Send
+                    </button>
                 </form>
+            </motion.div>
+
+            <motion.div
+                variants={slideIn('right', 'tween', 0.2, 1)}
+                className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+            >
+                <EarthCanvas/>
             </motion.div>
         </div>
   )
